41be87f438b9fb50836ca915af889132f0f29178
Richard <71195494+Richard-Egeli@users.noreply.github.com>
2022-12-25 18:13:04 +0100

    removed unnecessary hex addresses in enums

--- mcp342x.h
+++ mcp342x.h
@@ -8,29 +8,29 @@ typedef uint8_t (*mcp342x_com_fptr_t)(uint8_t addr, uint8_t* data, uint8_t len);
 typedef uint8_t (*mcp342x_delay_fptr_t)(uint32_t ms);
 
 typedef enum mcp342x_channel_enum {
-    MCP342X_CHANNEL_1 = 0x00,
-    MCP342X_CHANNEL_2 = 0x01,
-    MCP342X_CHANNEL_3 = 0x02,
-    MCP342X_CHANNEL_4 = 0x03,
+    MCP342X_CHANNEL_1,
+    MCP342X_CHANNEL_2,
+    MCP342X_CHANNEL_3,
+    MCP342X_CHANNEL_4,
 } mcp342x_channel_t;
 
 typedef enum mcp342x_mode_enum {
-    MCP342X_MODE_ONE_SHOT   = 0x00,
-    MCP342X_MODE_CONTINUOUS = 0x01,
+    MCP342X_MODE_ONE_SHOT,
+    MCP342X_MODE_CONTINUOUS,
 } mcp342x_mode_t;
 
 typedef enum mcp342x_gain_enum {
-    MCP342X_GAIN_1X = 0x00,
-    MCP342X_GAIN_2X = 0x01,
-    MCP342X_GAIN_4X = 0x02,
-    MCP342X_GAIN_8X = 0x03,
+    MCP342X_GAIN_1X,
+    MCP342X_GAIN_2X,
+    MCP342X_GAIN_4X,
+    MCP342X_GAIN_8X,
 } mcp342x_gain_t;
 
 typedef enum mcp342x_resolution_enum {
-    MCP342X_RESOLUTION_12_BIT = 0x00,
-    MCP342X_RESOLUTION_14_BIT = 0x01,
-    MCP342X_RESOLUTION_16_BIT = 0x02,
-    MCP342X_RESOLUTION_18_BIT = 0x03,
+    MCP342X_RESOLUTION_12_BIT,
+    MCP342X_RESOLUTION_14_BIT,
+    MCP342X_RESOLUTION_16_BIT,
+    MCP342X_RESOLUTION_18_BIT,
 } mcp342x_resolution_t;
 
 typedef enum mcp342x_err_enum {
